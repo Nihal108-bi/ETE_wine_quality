58e4b497117daee71615ecce6298e5c28b31ec48
Nihal108-bi <nihaljaisawal1@gmail.com>
2026-02-05 10:08:44 +0530

    data ingestion and validation completed

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ETE_wine_quality
+# ETE_wine_quality (Data Science Project )
+
+### Workflows--ML Pipeline
+
+1. Data Ingestion
+2. Data Validation
+2. Data Transformation
+3. Model Trainer
+4. Model Evaluation
+
+## Workflows
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration maanger in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.ETE_wine_quality import logger
+from src.ETE_wine_quality.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.ETE_wine_quality.pipeline.data_validation_pipeline import DataValidatioTrainingPipeline
 
-logger.info("Welcome to our custom logging data science")
+
+
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>>stage {STAGE_NAME} started <<<<<<<")
+    obj=DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
+
+STAGE_NAME="Data Validaton Stage"
+try:
+    logger.info(f">>>>>>>>>stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion=DataValidatioTrainingPipeline()
+    data_ingestion.initiate_data_validation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e  
